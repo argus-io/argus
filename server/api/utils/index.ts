e379a2442edc2f2a165e6a8b0212fea7dac69355
Arafat Al Mahmud <arafat.almahmud89@gmail.com>
2020-01-18 16:26:07 +0600

    Introduce typescript

--- server/api/utils/index.ts
+++ server/api/utils/index.ts
@@ -1,6 +1,7 @@
+import { Response } from 'express';
 const ErrorMessages = require("./errorMessages.js");
 
-const RouteExceptionHandler = (_, res) => {
+const RouteExceptionHandler = (_: any, res: Response) => {
     res
         .status(ErrorMessages.BadRequest.statusCode)
         .json(ErrorMessages.BadRequest)
